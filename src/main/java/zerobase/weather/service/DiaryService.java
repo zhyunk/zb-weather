42a1597faa4d63e3d8211ec9331e90b3acae4857
zhyunk <gimwlgus@gmail.com>
2023-05-21 23:36:51 +0900

    API 값 받아와서 변수에 저장
    URL 객체와 HttpURLConnection 객체를 활용해서 
    API 통신을 하고 결과를 Stirng 변수에 저장하는 실습을 했다.

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -3,6 +3,11 @@ package zerobase.weather.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
 
 @Service
@@ -11,12 +16,40 @@ public class DiaryService {
     private String apiKey;
 
     public void createDiary(LocalDate date, String text) {
-        getWeatherString();
+        // open weather 가져오기
+        String weatherData = getWeatherString();
+
+        // 받아온 날씨 json 파싱하기
+
+        // 파싱 된 데이터 + 일기 값 db에 넣기
     }
 
     private String getWeatherString() {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appId=" + apiKey;
-        System.out.println(apiUrl);
-        return "";
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
+
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
     }
 }
