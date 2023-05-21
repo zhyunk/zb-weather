98c5078eff2338b3d8326695fd113d9d3878d125
zhyunk <gimwlgus@gmail.com>
2023-05-21 23:19:34 +0900

    OPEN API URL TEST
    URI에 필요한 정보를 맞게 입력했는지 확인을 위한 URL 테스트

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,12 +1,22 @@
 package zerobase.weather.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
 public class DiaryService {
-    public void createDiary(LocalDate date, String text) {
+    @Value("${open-weather}")
+    private String apiKey;
 
+    public void createDiary(LocalDate date, String text) {
+        getWeatherString();
+    }
+
+    private String getWeatherString() {
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appId=" + apiKey;
+        System.out.println(apiUrl);
+        return "";
     }
 }
