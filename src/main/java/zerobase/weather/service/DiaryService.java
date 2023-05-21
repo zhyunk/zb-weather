d4435e8ae09ad6838ecf7005c4c1bb8602c26c4e
zhyunk <gimwlgus@gmail.com>
2023-05-21 23:58:54 +0900

    api 호출 결과 json parsing
    api 호출 결과를 json으로 파싱하여 Map에 필요한 정보만 담음

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,5 +1,8 @@
 package zerobase.weather.service;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +12,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DiaryService {
@@ -20,8 +25,10 @@ public class DiaryService {
         String weatherData = getWeatherString();
 
         // 받아온 날씨 json 파싱하기
+        Map<String, Object> parsedWeather = parseWeather(weatherData);
 
         // 파싱 된 데이터 + 일기 값 db에 넣기
+
     }
 
     private String getWeatherString() {
@@ -51,5 +58,28 @@ public class DiaryService {
         } catch (Exception e) {
             return "failed to get response";
         }
+    }
+
+    private Map<String, Object> parseWeather(String jsonString) {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+
+        } catch(ParseException e) {
+            throw new RuntimeException(e);
+        }
+        // {"coord":{"lon":126.9778,"lat":37.5683},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],"base":"stations","main":{"temp":289.58,"feels_like":289.24,"temp_min":287.84,"temp_max":290.81,"pressure":1010,"humidity":75},"visibility":8000,"wind":{"speed":1.54,"deg":310},"clouds":{"all":75},"dt":1684679372,"sys":{"type":1,"id":8105,"country":"KR","sunrise":1684613934,"sunset":1684665519},"timezone":32400,"id":1835848,"name":"Seoul","cod":200}
+        Map<String, Object> resultMap = new HashMap<>();
+
+        JSONObject mainData = (JSONObject) jsonObject.get("main");
+        resultMap.put("temp", mainData.get("temp"));
+
+        JSONObject weatherData = (JSONObject) jsonObject.get("weather");
+        resultMap.put("main", weatherData.get("main"));
+        resultMap.put("icon", weatherData.get("icon"));
+
+        return resultMap;
     }
 }
