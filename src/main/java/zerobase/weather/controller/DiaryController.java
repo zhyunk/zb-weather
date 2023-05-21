f7ff66d71bfc73ed166257ac49b11b3355412c3e
zhyunk <gimwlgus@gmail.com>
2023-05-21 22:43:44 +0900

    create diary 부분 controller 작성
    1. controller : view에서 request 받는 형태 작성
    2. service: controller와 소통 할 메서드 작성

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,7 +1,32 @@
 package zerobase.weather.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import zerobase.weather.service.DiaryService;
+
+import java.time.LocalDate;
 
 @Controller
 public class DiaryController {
+    private final DiaryService diaryService;
+
+    public DiaryController(DiaryService diaryService) {
+        this.diaryService = diaryService;
+    }
+
+    @PostMapping("/create/diary")
+    void createDiary(
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) // date 포맷 형식 지정해서 받음 ISO.DATE = yyyy-MM-dd = "2000-10-31"
+            LocalDate date,
+
+            @RequestBody
+            String text
+    ) {
+        diaryService.createDiary(date, text);
+    }
+
 }
--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -2,6 +2,11 @@ package zerobase.weather.service;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class DiaryService {
+    public void createDiary(LocalDate date, String text) {
+
+    }
 }
